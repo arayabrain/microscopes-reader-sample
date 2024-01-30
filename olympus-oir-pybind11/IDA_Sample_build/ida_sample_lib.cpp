1b5c73cb8455050ff840c5be477c0c99a1a4af0d
tuanna26 <tuanna@reactplus.com>
2024-01-30 14:04:52 +0700

    Fix pybind11 for output tiff file

--- olympus-oir-pybind11/IDA_Sample_build/ida_sample_lib.cpp
+++ olympus-oir-pybind11/IDA_Sample_build/ida_sample_lib.cpp
@@ -6,8 +6,11 @@
 #include "AreaImageSize.h"
 #include "FrameManager.h"
 #include "SystemInfo.h"
+#include "RoiCollection.h"
 
 PYBIND11_MAKE_OPAQUE(std::vector<IDA_AXIS_INFO>);
+PYBIND11_MAKE_OPAQUE(std::vector<CAxisPosition>);
+PYBIND11_MAKE_OPAQUE(std::vector<unsigned char>);
 
 PYBIND11_MODULE(ida_sample_lib, m) {
     pybind11::class_<CAxisInfo>(m, "AxisInfo")
@@ -22,6 +25,15 @@ PYBIND11_MODULE(ida_sample_lib, m) {
         .def("GetMax", &CAxis::GetMax)
         ;
 
+    pybind11::class_<CAxisPosition>(m, "AxisPosition")
+        .def("SetExit", &CAxisPosition::SetExit)
+        .def("SetPosition", &CAxisPosition::SetPosition)
+        .def("SetType", &CAxisPosition::SetType)
+        .def("GetExit", &CAxisPosition::GetExit)
+        .def("GetPosition", &CAxisPosition::GetPosition)
+        .def("GetType", &CAxisPosition::GetType)
+        ;
+
     pybind11::class_<CAreaImageSize>(m, "AreaImageSize")
         .def(pybind11::init<IDA_HACCESSOR, IDA_HAREA>())
         .def("GetX", &CAreaImageSize::GetX)
@@ -29,6 +41,41 @@ PYBIND11_MODULE(ida_sample_lib, m) {
         .def("Print", &CAreaImageSize::Print)
         ;
 
+    pybind11::class_<CRoi>(m, "Roi")
+        .def(pybind11::init<>())
+        .def("SetId", &CRoi::SetId)
+        .def("SetName", &CRoi::SetName)
+        .def("SetType", &CRoi::SetType)
+        .def("SetShape", &CRoi::SetShape)
+        .def("SetRotation", &CRoi::SetRoation)
+        .def("SetPoints", &CRoi::SetPoints)
+        .def("SetPanX", &CRoi::SetPanX)
+        .def("SetPanY", &CRoi::SetPanY)
+        .def("SetZoom", &CRoi::SetZoom)
+        .def("SetZ", &CRoi::SetZ)
+        .def("SetObjectId", &CRoi::SetObjectId)
+        .def("SetTransmissivity", &CRoi::SetTransmissivity)
+        .def("GetId", &CRoi::GetId)
+        .def("GetName", &CRoi::GetName)
+        .def("GetType", &CRoi::GetType)
+        .def("GetShape", &CRoi::GetShape)
+        .def("GetRotation", &CRoi::GetRotation)
+        .def("GetPoints", &CRoi::GetPoints)
+        .def("GetPanX", &CRoi::GetPanX)
+        .def("GetPanY", &CRoi::GetPanY)
+        .def("GetZoom", &CRoi::GetZoom)
+        .def("GetZ", &CRoi::GetZ)
+        .def("GetObjectId", &CRoi::GetObjectId)
+        .def("GetTransmissivity", &CRoi::GetTransmissivity)
+        .def("WriteRoi", &CRoi::WriteRoi);
+
+    pybind11::class_<CRoiCollection>(m, "RoiCollection")
+        .def(pybind11::init<IDA_HACCESSOR, IDA_HAREA, const wchar_t*, const wchar_t*>())
+        .def("Print", &CRoiCollection::Print)
+        .def("hasLineROI", &CRoiCollection::hasLineROI)
+        .def("hasPointROI", &CRoiCollection::hasPointROI)
+        .def("hasMultipointROI", &CRoiCollection::hasMultipointROI)
+        .def("hasMappingROI", &CRoiCollection::hasMappingROI);
     pybind11::class_<CChannelInfo>(m, "ChannelInfo")
         .def(pybind11::init<IDA_HACCESSOR, IDA_HAREA>())
         .def("GetChannelId", &CChannelInfo::GetChannelId)
@@ -40,22 +87,38 @@ PYBIND11_MODULE(ida_sample_lib, m) {
         .def("GetImageBody", &CFrameManager::GetImageBody)
         .def("ReleaseImageBody", &CFrameManager::ReleaseImageBody)
         .def("getBufferSize", &CFrameManager::getBufferSize)
+        .def("pucBuffer_to_WORD_TM", &CFrameManager::pucBuffer_to_WORD_TM)
+        .def("GetFramePosition", &CFrameManager::GetFramePosition)
+        .def("WriteFramePosition", &CFrameManager::WriteFramePosition)
         ;
 
     pybind11::class_<CSystemInfo>(m, "SystemInfo")
         .def(pybind11::init<IDA_HACCESSOR, IDA_HAREA>())
         .def("Print", &CSystemInfo::Print)
         ;
+
     pybind11::class_<IDA_AXIS_INFO>(m, "IDA_AXIS_INFO")
         .def(pybind11::init<>())
         .def_readwrite("nType", &IDA_AXIS_INFO::nType)
         .def_readwrite("nNumber", &IDA_AXIS_INFO::nNumber);
+
     pybind11::enum_<IDA_AxisType>(m, "IDA_AxisType")
         .value("IDA_AT_TIME", IDA_AxisType::IDA_AT_TIME)
         .value("IDA_AT_Z", IDA_AxisType::IDA_AT_Z)
-        .value("IDA_AT_LAMBDA", IDA_AxisType::IDA_AT_LAMBDA);
+        .value("IDA_AT_LAMBDA", IDA_AxisType::IDA_AT_LAMBDA)
+        ;
+
     pybind11::bind_vector<std::vector<IDA_AXIS_INFO>>(m, "VectorAxisInfo")
         .def(pybind11::init<int>());
+
+    pybind11::bind_vector<std::vector<CAxisPosition>>(m, "VectorAxisPosition")
+        .def(pybind11::init<>())
+        ;
+
+    pybind11::bind_vector<std::vector<unsigned char>>(m, "VectorUInt8")
+        .def(pybind11::init<>())
+        ;
+
     pybind11::class_<CMN_RECT>(m, "CMN_RECT")
         .def(pybind11::init<>())
         .def_readwrite("x", &CMN_RECT::x)
